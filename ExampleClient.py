1b6ded191c61d96ed4fa275073547b9dcffe457e
Dominic Orlando <81651703+dom500d@users.noreply.github.com>
2024-04-01 12:50:58 -0700

    Update ExampleClient.py
    Paho mqtt client new version needs callback api version argument for constructor.

--- ExampleClient.py
+++ ExampleClient.py
@@ -82,7 +82,7 @@ def on_message(client, userdata, msg):
 # using MQTT version 5 here, for 3.1.1: MQTTv311, 3.1: MQTTv31
 # userdata is user defined data of any type, updated by user_data_set()
 # client_id is the given name of the client
-client = paho.Client(client_id="", userdata=None, protocol=paho.MQTTv5)
+client = paho.Client(callback_api_version=paho.CallbackAPIVersion.VERSION1, client_id="", userdata=None, protocol=paho.MQTTv5)
 client.on_connect = on_connect
 
 
