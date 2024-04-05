2358212080e54f8e0d3485b665211dc6540d21e1
Dominic Orlando <81651703+dom500d@users.noreply.github.com>
2024-04-04 17:09:10 -0700

    Update InputTypes.py
    New version of Pydantic broke this

--- InputTypes.py
+++ InputTypes.py
@@ -1,12 +1,12 @@
-from pydantic import BaseModel, constr
+from pydantic import BaseModel, Field
 
 class NewPlayer(BaseModel):
-    lobby_name: constr(min_length=1, max_length=20)
-    team_name: constr(min_length=1, max_length=20)
-    player_name: constr(min_length=1, max_length=20)
+    lobby_name: str = Field(..., min_length=1, max_length=20)
+    team_name: str = Field(..., min_length=1, max_length=20)
+    player_name: str = Field(..., min_length=1, max_length=20)
 
 class Move(BaseModel):
-    move: constr(regex=r'^(UP|DOWN|LEFT|RIGHT)$')
+    move: str = Field(..., pattern=r'^(UP|DOWN|LEFT|RIGHT)$')
 
 class Start(BaseModel):
-    start: constr(regex=r'^(START)$')
+    start: str = Field(..., pattern=r'^(START)$')
